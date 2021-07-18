2d3799b8a1c08b452426b298a0d9208044b9b69c
Rogério Barreto <rogeriodelphi@gmail.com>
2021-07-18 17:50:33 -0300

    Update strings_e_bytes.py

--- strings_e_bytes.py
+++ strings_e_bytes.py
@@ -4,12 +4,19 @@
 def main():
     # definindo alguns valores iniciais
     b = bytes([0x41, 0x42, 0x43, 0x44])
+    print(b)
+
+    s = "Isto é uma String"
+    print(s)
 
     # TODO: Tente juntar os dois.
-
+    #print(b + s)
+    
     # TODO: Bytes e strings precisam ser apropriadamente encoded
-
+    print(s + b.decode('UTF-8'))
+    
     # TODO: Faça o encode da string como UTF-32
+    print(s.encode('UTF-32') + b)
 
 
 if __name__ == "__main__":
